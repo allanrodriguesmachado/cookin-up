f912de183e506691da113b46d29a00d63f8f3a0e
Allan Rodrigues <allan.rodrigues14@hotmail.com>
2025-07-30 13:07:57 -0300

    Alter

---
+++ src/components/ConteudoPrincipal.vue
@@ -0,0 +1,12 @@
+<template>
+    <main class="conteudo-prinicipal">
+        <section>
+            <span class="subtitulo-lg sua-lista-texto">
+                Sua lista:
+            </span>
+            <ul class="ingredientes-sua-lista">
+                
+            </ul>
+        </section>
+    </main>
+</template>
