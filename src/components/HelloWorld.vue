e327405e9d2c00b7e683f59be838e6c896a6f546
Allan Rodrigues <allan.rodrigues14@hotmail.com>
2025-07-30 10:41:25 -0300

    Alter

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,74 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+.banner {
+  padding: 4rem 7.5rem;
+  color: var(--creme);
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  column-gap: 3.25rem;
+}
+
+.logo {
+  height: 4.5rem;
+  margin-bottom: 3rem;
+}
+
+.frase-cabecalho {
+  margin-bottom: 2rem;
+}
+
+.texto-verde {
+  color: var(--verde-medio, #3D6D4A);
+}
+
+.foto-banner {
+  width: 35rem;
+}
+
+@media only screen and (max-width: 1300px) {
+  .banner {
+    padding: 4rem 3.75rem;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  .logo {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .banner {
+    padding: 4rem 1.5rem;
+  }
+
+  .foto-banner {
+    width: min(100%, 21.25rem);
+  }
+}
+
+</style>
